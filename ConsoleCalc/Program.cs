58c36fa2994cf257f68fba6709e133ca03d39a68
IrinaMatisik <matisik_dist@mail.ru>
2023-01-17 12:36:30 +0300

    Addition

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double resAdd = a + b;
+            Console.WriteLine("Result addition = " + resAdd);
+            Console.ReadKey();
+        }
+    }
+}
