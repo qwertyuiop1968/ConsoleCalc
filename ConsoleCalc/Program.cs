71d83d70d42a7ecb6782a7dd6e651dff959a4d09
qwertyuiop1968 <matisik_git@mail.ru>
2023-01-26 13:29:45 +0300

    Sin

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -21,6 +21,8 @@ namespace SimpleCalc
             Console.WriteLine("Result divisual = " + resDiv);
             double resMode = a % b;
             Console.WriteLine("Result mode = " + resMode);
+            double resSin = Math.Sin(a);
+            Console.WriteLine("Result sin = " + resSin);
             Console.ReadKey();
         }
     }
